5eb205832863a7e31192abca8a6b66448e018a7d
Nate Mendoza <nmendoza82rg@aol.com>
2018-09-19 14:54:05 -0700

    Working on Round2
    built level need to work with objects and their colliders and the game aspect. Also how to switch scenes

---
+++ Assets/My Creations/My Scripts/FallingLidScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingLidScript : MonoBehaviour {
+
+    public Transform lid;
+    Rigidbody lidBody;
+
+
+
+	// Use this for initialization
+	void Start () {
+        lid = GetComponent<Transform>();
+        lidBody = GetComponent<Rigidbody>();
+        lidBody.isKinematic = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lid.rotation.x == 0)
+        {
+            lidBody.isKinematic = true;
+        }
+
+	}
+}
